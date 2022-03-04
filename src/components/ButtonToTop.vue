a61d99fa68f58b2d4fc626ddc6a16a6e3a3aec64
Stanislav Danilov <dstanleysupreme@gmail.com>
2022-03-04 21:52:12 +0400

    added primitive logic to button

--- src/components/ButtonToTop.vue
+++ src/components/ButtonToTop.vue
@@ -1,21 +1,56 @@
 <template>
-	<button class="button-scroll-to-top" @click="scrollToTop">↑</button>
+	<transition name="button-to-top" appear>
+		<button
+			class="button-scroll-to-top"
+			v-show="isButtonActive"
+			@click="backToTop"
+		>
+			↑
+		</button>
+	</transition>
 </template>
 <script>
 export default {
-	methods: {
-		scrollToTop() {
-			window.scroll({ top: 0, behavior: 'smooth' })
-			// let maxHeight = Math.max(
-			// 	document.body.scrollHeight,
-			// 	document.documentElement.scrollHeight,
-			// 	document.body.offsetHeight,
-			// 	document.documentElement.offsetHeight,
-			// 	document.body.clientHeight,
-			// 	document.documentElement.clientHeight
-			// )
+	data() {
+		return {
+			currentOffset: 0,
+			isButtonActive: false,
+		}
+	},
+
+	created() {
+		window.addEventListener('scroll', () => {
+			const offsetY = window.pageYOffset
+			this.getPageHeight / 3 > offsetY
+				? (this.isButtonActive = false)
+				: (this.isButtonActive = true)
+		})
+	},
+
+	computed: {
+		getPageHeight() {
+			return Math.max(
+				document.body.scrollHeight,
+				document.documentElement.scrollHeight,
+				document.body.offsetHeight,
+				document.documentElement.offsetHeight,
+				document.body.clientHeight,
+				document.documentElement.clientHeight
+			)
 		},
 	},
+
+	methods: {
+		backToTop() {
+			window.scroll({ top: 0, behavior: 'smooth' })
+		},
+	},
+
+	// watch: {
+	// 	curretnOffset(val) {
+
+	// 	},
+	// },
 }
 </script>
 <style lang="sass">
@@ -30,4 +65,18 @@ export default {
 	right: 30px
 	font-size: 22px
 	z-index: 10
+
+.button-to-top-enter-from,
+.button-to-top-leave-to
+	opacity: 0
+	transform: translateX(10px)
+
+.button-to-top-enter-to,
+.button-to-top-leave-from
+	opacity: 1
+	transform: translateX(0)
+
+.button-to-top-enter-active,
+.button-to-top-leave-active
+	transition: all .4s ease
 </style>
