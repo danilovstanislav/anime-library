dea92cd8f74e7cd3173b7276483da9462258b32d
dnvstan <dstanleysupreme@gmail.com>
2022-04-12 15:01:22 +0400

    added scroll to top button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
 	<page-header />
 	<router-view />
+	<ButtonToTop />
 	<page-footer />
 </template>
 
 <script>
 import PageHeader from '@/components/PageHeader.vue'
+import ButtonToTop from '@/components/ButtonToTop.vue'
 import PageFooter from '@/components/PageFooter.vue'
 export default {
 	components: {
 		PageHeader,
 		PageFooter,
+		ButtonToTop,
 	},
 }
 </script>
--- src/components/ButtonToTop.vue
+++ src/components/ButtonToTop.vue
@@ -5,7 +5,11 @@
 			v-show="isButtonActive"
 			@click="backToTop"
 		>
-			↑
+			<svg class="button-scroll-to-top__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+				<path
+					d="M374.6 246.6C368.4 252.9 360.2 256 352 256s-16.38-3.125-22.62-9.375L224 141.3V448c0 17.69-14.33 31.1-31.1 31.1S160 465.7 160 448V141.3L54.63 246.6c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0l160 160C387.1 213.9 387.1 234.1 374.6 246.6z"
+				/>
+			</svg>
 		</button>
 	</transition>
 </template>
@@ -17,13 +21,12 @@ export default {
 		}
 	},
 
-	created() {
-		window.addEventListener('scroll', () => {
-			const offsetY = window.pageYOffset
-			this.getPageHeight / 3 > offsetY
-				? (this.isButtonActive = false)
-				: (this.isButtonActive = true)
-		})
+	mounted() {
+		window.addEventListener('scroll', this.showHideButton)
+	},
+
+	unmounted() {
+		window.removeEventListener('scroll', this.showHideButton)
 	},
 
 	computed: {
@@ -43,6 +46,13 @@ export default {
 		backToTop() {
 			window.scroll({ top: 0, behavior: 'smooth' })
 		},
+
+		showHideButton() {
+			let offsetY = window.pageYOffset
+			this.getPageHeight / 3 > offsetY
+				? (this.isButtonActive = false)
+				: (this.isButtonActive = true)
+		},
 	},
 }
 </script>
@@ -51,14 +61,31 @@ export default {
 	@include button
 	width: 50px
 	height: 50px
+	padding: 0
+	display: flex
+	align-items: center
+	justify-content: center
 	position: fixed
 	bottom: 5%
 	left: 5%
+	border: 3px solid $light-black-color
 	font-size: 22px
 	z-index: 10
 
 	@media(max-width: $screen-s-max)
 		display: none
+
+	&:hover
+		border: 3px solid $light-black-color
+		background-color: $main-color
+
+		.button-scroll-to-top__icon
+			fill: $light-black-color
+
+	&__icon
+		width: 25px
+		height: 25px
+		fill: #fff
 
 .button-to-top-enter-from,
 .button-to-top-leave-to
